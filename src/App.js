eb4ff409b2bf602f548d64d7b479dae7c23234d4
Jashanpreet Kaur Chahal <chahal63@sheridancollege.ca>
2025-01-14 09:45:13 -0500

    React router simple

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Alert from './components/Alert';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React, { useState } from 'react';
-
+import About from './components/About';
+import { BrowserRouter as Router, Route,Routes} from "react-router-dom";
  
 function App() {
   const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
@@ -38,13 +39,19 @@ function App() {
 
   return (
     <>
+    <Router>
     {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
     {/* <Navbar/> */}
-    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+    <Navbar title="TextUtils" aboutText="About" mode={mode} toggleMode={toggleMode} />
     <Alert alert= {alert} />
     <div className="container my-3">
-    <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/>
-    </div>
+          <Routes>
+            {/* good practice to use exact path because react can sometimes use partical matches as well. */}
+            <Route exact path="/about" element={<About/>} />
+            <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />} />
+          </Routes>
+        </div>
+    </Router>
     
     </> 
     
