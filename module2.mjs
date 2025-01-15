4a344211782e5c3459a04895faca50ee27aa0474
Jashanpreet Kaur Chahal <chahal63@sheridancollege.ca>
2025-01-15 09:02:02 -0500

    Updates

---
+++ module2.mjs
@@ -0,0 +1,12 @@
+const a="jashan";
+const b="harjeet";
+const c='alshifa';
+const d="komal";
+
+// exporting 'a' as default
+export default a;
+
+// exporting other variables as well but not as default.
+export {b};
+export {c};
+export {d};
